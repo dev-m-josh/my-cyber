27c63a181c326f34178722e0922c8271c9001bd6
joshua mutambuki <mutambukijoshua2@gmail.com>
2026-09-16 17:01:33 +0300

    Created sales table

---
+++ api/src/db/schema/sales.ts
@@ -0,0 +1,34 @@
+import {
+  pgTable,
+  uuid,
+  numeric,
+  varchar,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const sales = pgTable("sales", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  customerId: uuid("customer_id"),
+
+  totalAmount: numeric("total_amount", {
+    precision: 10,
+    scale: 2,
+  }).notNull(),
+
+  paymentMethod: varchar("payment_method", {
+    length: 30,
+  }).notNull(),
+
+  status: varchar("status", {
+    length: 30,
+  }).default("completed").notNull(),
+
+  createdAt: timestamp("created_at")
+    .defaultNow()
+    .notNull(),
+
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .notNull(),
+});
