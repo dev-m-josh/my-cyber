4c17881716842c254982f1df6e30061ab55c0214
joshua mutambuki <mutambukijoshua2@gmail.com>
2026-09-16 14:03:46 +0300

    Initialized the backend

---
+++ api/src/db/schema/customers.ts
@@ -0,0 +1,39 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const customers = pgTable("customers", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  firstName: varchar("first_name", {
+    length: 100,
+  }).notNull(),
+
+  lastName: varchar("last_name", {
+    length: 100,
+  }),
+
+  phone: varchar("phone", {
+    length: 30,
+  }),
+
+  email: varchar("email", {
+    length: 255,
+  }),
+
+  emailVerified: boolean("email_verified")
+    .default(false)
+    .notNull(),
+
+  createdAt: timestamp("created_at")
+    .defaultNow()
+    .notNull(),
+
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .notNull(),
+});
